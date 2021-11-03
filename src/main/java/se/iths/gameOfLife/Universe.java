92b4102a06749d9e772cf0a1c758c23038d5ac2f
Kabbej <kabbe.jartun@gmail.com>
2021-11-03 11:16:34 +0100

    first tests now passes after implementing getState in Universe

--- src/main/java/se/iths/gameOfLife/Universe.java
+++ src/main/java/se/iths/gameOfLife/Universe.java
@@ -3,14 +3,32 @@ import se.iths.gameOfLife.Cell.cellState;
 
 public class Universe {
 
+    private Cell [][] state;
+
 
 
     public Universe(cellState [][] cellStates){
+        state = new Cell[cellStates.length][];
 
+        for (int row = 0; row < cellStates.length; row++) {
+            state[row] = new Cell[cellStates[row].length];
+            for (int col = 0; col < cellStates[row].length; col++) {
+                state[row][col] = new Cell(cellStates[row][col]);
+            }
+        }
     }
 
 
+
     public cellState[][] getState() {
-        return null;
+        Cell.cellState[][] cellStates = new Cell.cellState[state.length][];
+
+        for (int row = 0; row < state.length; row++) {
+            cellStates[row] = new Cell.cellState[state[row].length];
+            for (int col = 0; col < state[row].length; col++) {
+                cellStates[row][col] = state[row][col].getState();
+            }
+        }
+        return cellStates;
     }
 }
